b1de4e699a7c5c056483d89078e954f7894b8e35
AKASH021001-0202 <nakashn007@gmail.com>
2024-03-28 12:29:51 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css';
 import React from 'react';
 
-function App() {
-  const features1 = [
+
+const freePlan = {
+  method: "Free",
+  price: "$0/month",
+  features: [
     { name: "Single User", isCorrect: true },
     { name: "50GB Storage", isCorrect: true },
     { name: "Unlimited Public Projects", isCorrect: true },
@@ -11,9 +14,13 @@ function App() {
     { name: "Dedicated Phone Support", isCorrect: false },
     { name: "Free Subdomain", isCorrect: false },
     { name: "Monthly Status Reports", isCorrect: false }
-  ]; // Features for the first card
+  ]
+};
 
-  const features2 = [
+const plusPlan = {
+  method: "Plus",
+  price: "$9/month",
+  features: [
     { name: "5 Users", isCorrect: true },
     { name: "50GB Storage", isCorrect: true },
     { name: "Unlimited Public Projects", isCorrect: true },
@@ -22,9 +29,13 @@ function App() {
     { name: "Dedicated Phone Support", isCorrect: true },
     { name: "Free Subdomain", isCorrect: true },
     { name: "Monthly Status Reports", isCorrect: false }
-  ]; // Features for the second card
+  ]
+};
 
-  const features3 = [
+const proPlan = {
+  method: "Pro",
+  price: "$49/month",
+  features: [
     { name: "Unlimited Users", isCorrect: true },
     { name: "50GB Storage", isCorrect: true },
     { name: "Unlimited Public Projects", isCorrect: true },
@@ -33,13 +44,15 @@ function App() {
     { name: "Dedicated Phone Support", isCorrect: true },
     { name: "Free Subdomain", isCorrect: true },
     { name: "Monthly Status Reports", isCorrect: true }
-  ]; // Features for the third card
+  ]
+};
 
+function App() {
   return (
     <div className="container">
-      <Cards method="Free" price="$0/month" features={features1} />
-      <Cards method="Plus" price="$9/month" features={features2} />
-      <Cards method="Pro" price="$49/month" features={features3} />
+      <Cards name={freePlan} />
+      <Cards name={plusPlan} />
+      <Cards name={proPlan} />
     </div>
   );
 }
@@ -47,12 +60,15 @@ function App() {
 export default App;
 
 function Cards(props) {
+  // console.log(props)
+  const plan = props.name;
+ 
   return (
     <div className='cards'>
-      <h5 className='price-method'>{props.method}</h5>
-      <h1 className='price'>{props.price}</h1>
+      <h5 className='price-method'>{plan.method}</h5>
+      <h1 className='price'>{plan.price}</h1>
       <ul className='list-feature'>
-        {props.features.map((feature, index) => (
+        {plan.features.map((feature, index) => (
           <li key={index} className={feature.isCorrect === false ? "red-text" : ""}>
             {feature.isCorrect ? "✔" : "✖"} &nbsp;{feature.name} 
           </li>
